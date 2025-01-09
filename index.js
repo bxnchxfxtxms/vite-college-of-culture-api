e2ba8b67976b1997034b1ac7952bbab8fdcd3f2b
Dmitriy Shumeikin <bunchomuncho@gmail.com>
2025-01-09 11:20:06 +0300

    third commit

--- index.js
+++ index.js
@@ -6,12 +6,36 @@ const router = require('./router.js');
 
 const { PORT = 3000, BASE_PATH } = process.env;
 
+const allowedCors = [
+  'http://колледжльвова.рф',
+  'https://колледжльвова.рф',
+  'localhost:3000',
+];
+
 const app = express();
 
 app.use(serveStatic(path.join(__dirname, '../vite-college-of-culture-frontend/src')));
 // app.use(serveStatic(path.join(__dirname, 'publicTest')));
 
 // app.use(express.static('public'))
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
